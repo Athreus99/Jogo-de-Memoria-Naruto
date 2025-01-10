b8093be967befdba20ca4c424a0a853fb796f421
Athreus99 <matheus.gouvea360@gmail.com>
2025-01-09 22:37:27 -0300

    configurações de deploy

--- jogo-memoria-naruto/vite.config.ts
+++ jogo-memoria-naruto/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Jogo-de-Memoria-Naruto/',  // Substitua 'seu-repo' pelo nome do seu repositório
+  base: '/jogo-de-Memoria-Naruto/',  
 });
